a9a225a6ac8be228cbf59cc10d6f2c3f054dd3ff
Egorov Anatolii <silkroadbum@mail.ru>
2024-04-13 19:57:05 +0300

    initialization project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+
+function App() {
+  return (
+    <>
+      <Button htmlType={"button"} />
+    </>
+  );
+}
+
+export default App;
