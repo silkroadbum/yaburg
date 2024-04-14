358fd76e405e64786fcad3da3aa2969508b72934
Egorov Anatolii <silkroadbum@mail.ru>
2024-04-14 22:12:15 +0300

    fix layout

--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,12 +1,11 @@
 import { FC, PropsWithChildren } from "react";
 import AppHeader from "@/components/app-header/app-header";
-import styles from "./main-layout.module.scss";
 
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
       <AppHeader />
-      <main className={styles.container}>{children}</main>
+      {children}
     </>
   );
 };
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -2,12 +2,15 @@ import { FC } from "react";
 import BurgerIngridients from "@/components/burger-ingridients/burger-ingridients";
 import BurgerConstructor from "@/components/burger-constructor/burger-constructor";
 import MainLayout from "@/layouts/main-layout/main-layout";
+import styles from "./main-page.module.scss";
 
 const MainPage: FC = () => {
   return (
     <MainLayout>
-      <BurgerIngridients />
-      <BurgerConstructor />
+      <main className={styles.container}>
+        <BurgerIngridients />
+        <BurgerConstructor />
+      </main>
     </MainLayout>
   );
 };
