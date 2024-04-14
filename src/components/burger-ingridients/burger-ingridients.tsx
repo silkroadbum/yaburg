f34e4498c86af9e92aa2c36e6cd0b6f7852ecf50
Egorov Anatolii <silkroadbum@mail.ru>
2024-04-15 01:10:34 +0300

    add scrollbar in ingridients component

--- src/components/burger-ingridients/burger-ingridients.tsx
+++ src/components/burger-ingridients/burger-ingridients.tsx
@@ -1,7 +1,9 @@
 import { FC, useState } from "react";
+import cn from "classnames";
 import TabPanel from "./tab-panel/tab-panel";
 import IngridientsList from "./ingridients-list/ingridients-list";
 import { data } from "@/utils/data";
+import styles from "./burger-ingridients.module.scss";
 
 const BurgerIngridients: FC = () => {
   const [activeTab, setActiveTab] = useState<string>("bun");
@@ -11,12 +13,14 @@ const BurgerIngridients: FC = () => {
   const mainIngridients = data.filter((item) => item.type === "main");
 
   return (
-    <div className="mt-10">
+    <div className={cn("mt-10", [styles.ingridients])}>
       <h2 className="text text_type_main-large mb-5">Соберите бургер</h2>
       <TabPanel activeTab={activeTab} onClick={setActiveTab} />
-      <IngridientsList title="Булки" ingridients={bunIngridients} />
-      <IngridientsList title="Соусы" ingridients={sauseIngridients} />
-      <IngridientsList title="Начинки" ingridients={mainIngridients} />
+      <div className={styles.ingridients_lists}>
+        <IngridientsList title="Булки" ingridients={bunIngridients} />
+        <IngridientsList title="Соусы" ingridients={sauseIngridients} />
+        <IngridientsList title="Начинки" ingridients={mainIngridients} />
+      </div>
     </div>
   );
 };
