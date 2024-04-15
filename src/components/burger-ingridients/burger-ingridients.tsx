edba2aa4d1ea0c92d839b89c143b7e48f5f25d2f
Egorov Anatolii <silkroadbum@mail.ru>
2024-04-15 22:39:50 +0300

    fix height

--- src/components/burger-ingridients/burger-ingridients.tsx
+++ src/components/burger-ingridients/burger-ingridients.tsx
@@ -16,7 +16,7 @@ const BurgerIngridients: FC = () => {
     <div className={cn("mt-10", [styles.ingridients])}>
       <h2 className="text text_type_main-large mb-5">Соберите бургер</h2>
       <TabPanel activeTab={activeTab} onClick={setActiveTab} />
-      <div className={styles.ingridients_lists}>
+      <div className={cn("custom-scroll", styles.ingridients_lists)}>
         <IngridientsList title="Булки" ingridients={bunIngridients} />
         <IngridientsList title="Соусы" ingridients={sauseIngridients} />
         <IngridientsList title="Начинки" ingridients={mainIngridients} />
