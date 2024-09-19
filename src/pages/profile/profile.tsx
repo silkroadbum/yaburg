8fb3a8e3b8e53f263fc7e4c3cc4f379edaa2cab8
Egorov Anatolii <silkroadbum@mail.ru>
2024-09-20 00:03:03 +0300

    добавлено состояние формы для профиля

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -2,15 +2,20 @@ import cn from "classnames";
 import styles from "./profile.module.scss";
 import { EmailInput, Input, PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
 import { ChangeEvent, useState } from "react";
-import { useAppDispatch } from "@/services/hooks";
+import { useAppDispatch, useAppSelector } from "@/services/hooks";
 import { logout } from "@/services/user/actions";
+import { NavLink } from "react-router-dom";
+import { RoutePath } from "@/constants/router";
+import { selectUser } from "@/services/user/selectors";
 
 export const Profile = () => {
-  const [email, setEmail] = useState("123");
+  const user = useAppSelector(selectUser);
+  const [formData, setFormData] = useState({ email: user?.email ?? "", password: "", name: user?.name ?? "" });
   const dispatch = useAppDispatch();
 
   const onChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
-    setEmail(target.value);
+    const { name, value } = target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   const onClickExit = () => {
@@ -21,7 +26,13 @@ export const Profile = () => {
     <div className={styles.container}>
       <nav>
         <ul className="text text_type_main-medium mb-20">
-          <li className={styles.list_item}>Профиль</li>
+          <li className={styles.list_item}>
+            <NavLink to={RoutePath.profile}>
+              {({ isActive }) => (
+                <span className={isActive ? "text_color_primary" : "text_color_inactive"}>Профиль</span>
+              )}
+            </NavLink>
+          </li>
           <li className={cn("text_color_inactive", styles.list_item)}>История заказов</li>
           <li className={cn("text_color_inactive", styles.list_item)} onClick={onClickExit}>
             Выход
@@ -32,9 +43,23 @@ export const Profile = () => {
         </p>
       </nav>
       <form className={styles.form}>
-        <Input icon={"EditIcon"} extraClass="mb-6" placeholder="Имя" value={""} onChange={() => console.log("имя")} />
-        <EmailInput isIcon extraClass="mb-6" placeholder="Логин" value={email} onChange={onChange} />
-        <PasswordInput icon={"EditIcon"} value={""} onChange={() => console.log("password")} />
+        <Input
+          icon={"EditIcon"}
+          extraClass="mb-6"
+          placeholder="Имя"
+          value={formData.name}
+          onChange={onChange}
+          name="name"
+        />
+        <EmailInput
+          isIcon
+          extraClass="mb-6"
+          placeholder="Логин"
+          value={formData.email}
+          onChange={onChange}
+          name="email"
+        />
+        <PasswordInput icon={"EditIcon"} value={formData.password} onChange={onChange} name="password" />
       </form>
     </div>
   );
